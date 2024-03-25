ff67bf8171cfcabd9a662806370bf02a0d0801d0
Nvm35 <twikk3@gmail.com>
2024-03-25 22:58:59 +0500

    45

--- app/page.jsx
+++ app/page.jsx
@@ -5,7 +5,7 @@ const HomePage = () => {
   return (
     <div>
       <h1 classname="text-3xl">Welcome</h1>
-      <Link href="/properties">Show Prop</Link>
+      <Link href="/properties">Show Props</Link>
     </div>
   );
 };
