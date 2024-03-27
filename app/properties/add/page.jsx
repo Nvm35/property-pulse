a630c5aca162b23eacb5014656946c793aa2c165
Nvm35 <twikk3@gmail.com>
2024-03-27 21:43:46 +0500

    fixed classes

--- app/properties/add/page.jsx
+++ app/properties/add/page.jsx
@@ -1,22 +1,5 @@
-import PropertyCard from "@/components/PropertyCard";
-import properties from "@/properties.json";
-
-const PropertiesPage = () => {
-  return (
-    <section class="px-4 py-6">
-      <div class="container-xl lg:container m-auto px-4 py-6">
-        {properties.length === 0 ? (
-          <p>No props found</p>
-        ) : (
-          <div class="grid grid-cols-1 md:grid-cols-3 gap-6">
-            {properties.map((property) => (
-              <PropertyCard key={property._id} property={property} />
-            ))}
-          </div>
-        )}
-      </div>
-    </section>
-  );
+const PropertiesAddPage = () => {
+  return <div>ASD</div>;
 };
 
-export default PropertiesPage;
+export default PropertiesAddPage;
